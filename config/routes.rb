04ef303f899c914cb1749ca8b53686158ee497ca
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-01-24 18:47:04 -0600

    routes, auth, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  
+  get 'sessions/new'
+  
   resources :entries
   resources :users
+  resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+
+  get 'welcome/index'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
