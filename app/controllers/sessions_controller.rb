1d9914f41ab1d52abc8928c1facf8c801c23b9b8
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-02-01 21:22:55 -0600

    redirect after login/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'Logged in!'
+      redirect_to new_entry_url
     else
       flash.now.alert = 'Email or password is invalid'
       render :new
